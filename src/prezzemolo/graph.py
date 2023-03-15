aebb2c1af3c7f41cd469c3cc850f86dca7a50c9e
Neal Chambers <macanudo@yahoo.com>
2023-03-15 16:42:09 +0900

    Simplifiy __gt__
    Co-authored-by: eprbell <77937475+eprbell@users.noreply.github.com>

--- src/prezzemolo/graph.py
+++ src/prezzemolo/graph.py
@@ -36,11 +36,7 @@ class _ShortestDistance(Generic[ValueType]):
         return self.distance < other.distance
 
     def __gt__(self, other: object) -> bool:
-        if not other:
-            return False
-        if not isinstance(other, _ShortestDistance):
-            raise TypeError(f"Operand is not class _ShortestDistance: {repr(other)}")
-        return self.distance > other.distance
+        return not self.__lt__(other)
 
 
 class Graph(Generic[ValueType]):
